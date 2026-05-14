7771231092b43ddcf514a76ac5dba1f937124197
TAD 11 <gspacewhale23@gmail.com>
2026-05-14 17:32:32 +1000

    WIP: movement system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0f;
+
+    private float MovementY;
+    private float MovementX;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
-    }
+    }   
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, speed);
     }
 }
