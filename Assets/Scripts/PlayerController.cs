fcbf9a53a2fa152b68362bcb7975f1786de8c9f3
TAD 11 <gspacewhale23@gmail.com>
2026-05-15 12:40:03 +1000

    Feature:addedX movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0f;
+    public float speed = 5f;
+   
 
     private float MovementY;
     private float MovementX;
+
+    public Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,11 +21,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        Vector2 newVelcoity = new Vector2(MovementX * speed, rb.linearVelocity.y);
+        rb.linearVelocity = newVelcoity;
+    }   
 
-    void OnMove(InputValue movementValue)
+    public void onMove(InputAction.CallbackContext context)
     {
-        GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, speed);
+        MovementX = context.ReadValue<Vector2>().x;
     }
 }
